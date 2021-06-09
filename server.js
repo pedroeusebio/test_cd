e8a1aa694ee74edf4822e0a6838892e863ee8288
Pedro Eusebio <pedrosantos95@gmail.com>
2021-06-09 15:20:48 -0300

    creating a basic server for testing cd workflow

---
+++ server.js
@@ -0,0 +1,11 @@
+"use strict";
+
+const express = require("express");
+
+const PORT = 5000;
+const HOST = "0.0.0.0";
+
+express()
+  .get("/", (req, res) => res.send("Hello World"))
+  .listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
